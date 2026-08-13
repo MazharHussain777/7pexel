2dcb67b9aa9b8ad5ac9ed030b6d6f5ec3413868c
Mazhar Hussain <MazharHussain137913@gmail.com>
2026-08-13 12:24:27 +0500

    files

--- app/reviews/category/[category]/page.tsx
+++ app/reviews/category/[category]/page.tsx
@@ -1,4 +1,7 @@
 // app/reviews/category/[category]/page.tsx
+
+export const dynamic = 'force-dynamic';
+
 import { notFound } from "next/navigation";
 import type { Metadata } from "next";
 import Link from "next/link";
